b3e9eb43d4d12bc9bf317843c84c0973fd79da60
peterxiaoen <13211661891@163.com>
2017-11-18 20:25:29 -0800

    前 ++ 和后 ++的区别

--- 3.c
+++ 3.c
@@ -11,9 +11,13 @@ int main(int argc, char *argv[])
 {
 	int buff[] =  {1, 2, 3};
 	int *p = buff;
+	printf(" %d\n", *p);
 	*p++ += 123;
-
+	// 前 ++ 和后 ++的区别
+	// printf("size %d\n", sizeof(buff));
 	printf("%d,%d\n", *p, *++p);
+	printf("%d\n", *p);
+	printf("buff %d\n", buff[0]);
 
 	return 0;
 
